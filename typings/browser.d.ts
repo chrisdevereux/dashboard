6057ef7ab6cc4b751ebacc560755dd204add99ca
Chris Devereux <devereux.chris@gmail.com>
2015-11-10 14:23:26 +0000

    Add ES6 typings

--- typings/browser.d.ts
+++ typings/browser.d.ts
@@ -1,4 +1,5 @@
 /// <reference path="browser/ambient/chai/chai.d.ts" />
+/// <reference path="browser/ambient/es6-shim/es6-shim.d.ts" />
 /// <reference path="browser/ambient/mocha/mocha.d.ts" />
 /// <reference path="browser/ambient/react-addons-test-utils/react-addons-test-utils.d.ts" />
 /// <reference path="browser/ambient/react-dom/react-dom.d.ts" />
--- typings/main.d.ts
+++ typings/main.d.ts
@@ -1,4 +1,5 @@
 /// <reference path="main/ambient/chai/chai.d.ts" />
+/// <reference path="main/ambient/es6-shim/es6-shim.d.ts" />
 /// <reference path="main/ambient/mocha/mocha.d.ts" />
 /// <reference path="main/ambient/react-addons-test-utils/react-addons-test-utils.d.ts" />
 /// <reference path="main/ambient/react-dom/react-dom.d.ts" />
